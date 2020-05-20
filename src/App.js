c36b96178a9b1843a7ec474f171a601231100073
shahar <shaharp2@gmail.com>
2020-05-20 13:51:54 +0300

    try

--- src/App.js
+++ src/App.js
@@ -6,13 +6,10 @@ import './App.css';
 
 import Home from './pages/Home.js';
 import Login from './pages/Login.js';
-<<<<<<< HEAD
 import Signup from './pages/Signup.js';
 import { BoardApp } from './pages/Boards';
-=======
 import About from './pages/About.js';
 import ProgressBar from './cmps/ProgressBar'
->>>>>>> 674cdb6b54f5f0908610f833a4a9394097b46cbb
 
 
 function App() {
@@ -25,19 +22,16 @@ function App() {
           <Link to="/signup">signup</Link>
         </nav>
         <Switch>
-<<<<<<< HEAD
           <Route path="/" component={Home} exact />
           <Route path="/board" component={BoardApp} exact />
           <Route path="/login" component={Login} exact />
           <Route path="/signup" component={Signup} exact />
 
-=======
           <Route path="/" component={Home} exact/>
           <Route path="/about" component={About} exact/>
           <Route path="/login" component={Login} exact/>
           {/* <Route path="/" component={About} exact/> */}
           <Route path="/progress" component={ProgressBar} exact/>
->>>>>>> 674cdb6b54f5f0908610f833a4a9394097b46cbb
         </Switch>
       </Router>
     </div>
