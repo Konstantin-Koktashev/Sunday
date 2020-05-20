9e19c9b148555b8218e4ea4764727d402a1b4bac
AbirNadav <pakakaw41@gmail.com>
2020-05-20 13:49:45 +0300

    123

--- src/cmps/SideNav.jsx
+++ src/cmps/SideNav.jsx
@@ -1,32 +1,31 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function SideNav() {
-    return (
-        <div>
-            <NavLink to='/home' className='logo'>
-        <img src=''></img>
+  return (
+    <div>
+      <NavLink to="/home" className="logo">
+        <img src=""></img>
         Logo
-            </NavLink>
-            <NavLink to='/user/notifications' className='notifications'>
-        <img src=''></img>
-            </NavLink>
-            <NavLink to='/user/inbox' className='inbox'>
-        <img src=''></img>
-            </NavLink>
-            <NavLink to='/user/myWeek' className='my-week'>
-        <img src=''></img>
-            </NavLink>
-            <NavLink to='/user/invite' className='invite'>
-        <img src=''></img>
-            </NavLink>
-            <NavLink to='/user/search' className='search'>
-        <img src=''></img>
-            </NavLink>
-            <NavLink to='/user/profile' className='search'>
-        <img src=''></img>
-            </NavLink>
-            
-        </div>
-    )
+      </NavLink>
+      <NavLink to="/user/notifications" className="notifications">
+        <img src=""></img>
+      </NavLink>
+      <NavLink to="/user/inbox" className="inbox">
+        <img src=""></img>
+      </NavLink>
+      <NavLink to="/user/myWeek" className="my-week">
+        <img src=""></img>
+      </NavLink>
+      <NavLink to="/user/invite" className="invite">
+        <img src=""></img>
+      </NavLink>
+      <NavLink to="/user/search" className="search">
+        <img src=""></img>
+      </NavLink>
+      <NavLink to="/user/profile" className="search">
+        <img src=""></img>
+      </NavLink>
+    </div>
+  );
 }
