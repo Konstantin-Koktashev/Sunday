3814c0a170f087181c82d54a001052717c3b87b6
Konstantin-Koktashev <lexxtalionis33@gmail.com>
2020-05-21 00:00:27 +0300

    before pull

--- src/pages/Inbox.js
+++ src/pages/Inbox.js
@@ -1,56 +1,83 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-
 class Inbox extends Component {
 
     checkUserHistory = () => {
         const { board } = this.props.userBoards
         const currUserId = this.props.currUser.loggedInUser._id
-        var totalUserHistory=[]
+        var totalUserHistory = []
         board.forEach(board => {
-            let userPersonalHistory  = board.history.filter(history => {
-              return history.users.filter(user => +user._id === currUserId)
+            let userPersonalHistory = board.history.filter(history => {
+                return history.users.filter(user => +user._id === currUserId) && history.type === 'status'
             })
             totalUserHistory.push(...userPersonalHistory)
         });
         return totalUserHistory
     }
+    // checkAssociatedTaskIds=()=>{
+    //     const { board } = this.props.userBoards
+    //     board.forEach(board => {
+    //         let userPersonalHistory = board.tasks.filter(task => {
+    //             return task.filter(user => +user._id === currUserId) && history.type === 'status'
+    //         })
+    //         totalUserHistory.push(...userPersonalHistory)
+    //     });
+    // }
 
     componentDidMount() {
     }
     componentDidUpdate() { }
 
     render() {
-        const userHistory=this.checkUserHistory()
-        const HistoryToRender={
-            statusMsg:userHistory,
+        const userHistory = this.checkUserHistory()
 
-        }
+  
         console.log("Inbox -> render -> userHistory", userHistory)
         return (
             <div className='Inbox'>
-                {userHistory.map(history=>{
-                    return (<article className='user-history'>
-                        <section className='history-header'>
-                            <div className='user-logo'></div>
+                <h2>Inbox</h2>
+                {userHistory.map(history => {
+                console.log("Inbox -> render -> history", history)
+                    return (<article className='user-history flex col'>
+                        <div className='complete-Task Btn'>V</div>
+                        <section className='history-header flex'>
+                            <div className='user-logo'>
+                           
+                            </div>
                             <div className='updating-user'>
-                            <div className='history-origin'></div>
+                                {history.name}
+                                
+                                <div className='history-origin'>
+                                    {history.path}
+                                </div>
+                                <div>11 22 33</div>
                             </div>
                         </section>
                         <section className='update-msg'>
-                            
+                        {history.defaultMsg}
+                <span className='prev-value-inbox'> {history.prevValue}</span>
+                <span className='arrow-logo'>>{}</span>
+                
+                <span className='next-value-inbox'>{history.nextValue}</span>
                         </section>
-                        <div className="flex">
-
-                        </div>
-                        {history.descryption}
+                        <section className='like-reply-btns'>
+                        <button className='reply'>Reply</button>
+                        <button className='like'> >Like</button>
+                        </section>
+                        <section className='like-reply-btns'>
+                        <button className='reply'>Great Job!</button>
+                        <button className='like'> >Thanks I'll take it from here</button>
+                        <button className='like'> >Nice Work! Whats next?</button>
+                        </section>
                     </article>)
+                    
                 })}
             </div>
         )
     }
 }
+
 
 const mapStateToProps = (state) => ({
     userBoards: state.userBoards,
--- src/reducers/BoardReducer.js
+++ src/reducers/BoardReducer.js
@@ -220,13 +220,286 @@ const initialState = {
                     _id: "1234",
                 }],
                 lastUpdate: Date.now(),
+                descryption: "status was Change ",
+                nextValue: 'urgent',
+                prevValue: 'Low',
+                assignedTo: 'Abir',
+                createdBy: 'Kosta',
+                defaultMsg: 'status was Change ',
+                path: 'Sunday/FrontEnd/CreatApp',
+                assosicatedTaskId:'2222'
+            },
+            {
+                type: 'priority',
+                name: "name",
+                users: [{
+                    userName: "user1412",
+                    _id: "1234",
+                }, {
+                    userName: "user14as12",
+                    _id: "1234",
+                }],
+                lastUpdate: Date.now(),
+                descryption: "Priority was Changed ",
+                prevValue: 'medium',
+                prevValue: 'critical',
+                assignedTo: 'Abir',
+                createdBy: 'Kosta',
+                defaultMsg: 'Priority changed ',
+                historyPath: 'Sunday/FrontEnd/CreatApp',
+                assosicatedTaskId:'111'
+            },
+            {
+                type: 'people',
+                name: "name",
+                users: [{
+                    userName: "user1412",
+                    _id: "1234",
+                }, {
+                    userName: "user14as12",
+                    _id: "1234",
+                }],
+                lastUpdate: Date.now(),
+                descryption: "taskAssigned To",
+                prevValue: [{ userName: 'Kosta' }, { userName: 'Shahar' }],
+                nextValue: [{ userName: 'Kosta' }, { userName: 'Shahar' }, { userName: 'Abir' }],
+                assignedTo: 'Abir',
+                createdBy: 'Kosta',
+                defaultMsg: 'Assigned To',
+                historyPath: 'Sunday/FrontEnd/CreatApp',
+            },
+        ] //history objects
+    },
+    {
+        // BOARD OBJECT
+        _id: "1",
+        name: "Board 1",
+        createdAt: 1589990735884,
+        // Aggregation
+        admins: [{
+            userName: "user1412",
+            // fullName
+            _id: "1",
+        }], // Min users
+        users: [{
+            _id: "1",
+            userName: "user1412",
+            fullName: " full name1412",
+            password: "password", // NO PASSWORD
+            isAdmin: true,
+            imgUrl: "www.img.com",
+            lastSeen: "today", // timestamp
+            loggedAmount: 2,
+            location: "tel aviv",
+            notifications: [], //notification object
+            boards: [] // Boards ids
+        },
+        {
+            userName: "user112",
+            fullName: " full name1123",
+            password: "password",
+            _id: "1",
+            isAdmin: "true",
+            imgUrl: "www.img.com",
+            lastSeen: "today",
+            loggedAmount: 2,
+            location: "tel aviv",
+            notifications: [], //notification object
+            boards: [] // Boards ids
+        },
+        {
+            userName: "user2",
+            fullName: " full name2",
+            password: "password",
+            _id: "1",
+            isAdmin: "true",
+            imgUrl: "www.img.com",
+            lastSeen: "today",
+            loggedAmount: 2,
+            location: "tel aviv",
+            notifications: [], //notification object
+            boards: [] // Boards ids
+        }], // Min users
+        groups: [
+            {
+                _id: 123,
+                name: "",
+                createdAt: "date",
+                // Aggregation
+                tasks: [{
+                    _id: 2222,
+                    taskTitle: "Todo",
+                    createdAt: "date",
+                    // Aggregation
+                    users: [
+                        { _id: 1234, name: 'shahar' }
+                    ], // Min users
+                    // Inside Tas
+                    columns: [], //  Columns Objects
+                    updates: [], // updates objects
+                    notes: [], // Notes objects
+                    people: [],
+                    status: 'Done',
+                    priority: 'urgent',
+                    DueDate: '15.02',
+                    budget: '150',
+                    text: 'text about task',
+                    link: ''
+                },
+                {
+                    _id: 111,
+                    taskTitle: "Mess",
+                    createdAt: "date",
+                    // Aggregation
+                    users: [], // Min users
+                    // Inside Tas
+                    columns: [], //  Columns Objects
+                    updates: [], // updates objects
+                    notes: [], // Notes objects
+                    people: [],
+                    status: '',
+                    priority: '',
+                    DueDate: '',
+                    budget: '',
+                    text: '',
+                    link: ''
+                },
+                {
+                    _id: 12333,
+                    taskTitle: "withus",
+                    createdAt: "date",
+                    // Aggregation
+                    users: [], // Min users
+                    users: [{
+                        userName: "user1412",
+                        _id: "1",
+                    }], // Min users
+                    // Inside Tas
+                    updates: [{
+                        user: {
+                            userName: "user1412",
+                            _id: "1",
+                        },
+                        lastUpdate: Date.now(),
+                        description: "I changed all",
+                        imgUrl: "",
+                        aboutUser: 1234
+                    }], // updates objects
+                    notes: [{
+                        name: "name",
+                        description: "value",
+                        user: {
+                            userName: "user1412",
+                            _id: "1",
+                        },
+                        lastUpdate: Date.now()
+                    }], // Notes objects
+                    people: [
+                        {
+                            userName: "user1412",
+                            _id: "1",
+                        },
+                        {
+                            userName: "user1412",
+                            _id: "1",
+                        }, {
+                            userName: "user1412",
+                            _id: "1",
+                        }
+                    ],
+                    columns: [{ status: 'done' }, { priority: 'important' }, { dueDate: 'tommarow' }, { budget: 100 }, { text: 'hey' }, { link: '' }]
+                }
+                ], // Task object
+                color: 'blue',
+                lastUpdatedAt: ""
+            }
+        ], // _Group Objects
+        // Hard coded
+        color: 'red',
+        history: [
+            {
+                _id: 123,
+                name: "",
+                createdAt: "date",
+                // Aggregation
+                tasks: [{
+                    _id: 2222,
+                    taskTitle: "Todo",
+                    createdAt: "date",
+                    // Aggregation
+                    users: [
+                        { _id: 1234 }
+                    ], // Min users
+                    // Inside Tas
+                    columns: [], //  Columns Objects
+                    updates: [], // updates objects
+                    notes: [], // Notes objects
+                    people: [],
+                    status: 'undone',
+                    priority: 'urgent',
+                    DueDate: '15.02',
+                    budget: '1540',
+                    text: 'text about task 31142',
+                    link: ''
+                },
+                {
+                    _id: 111,
+                    taskTitle: "Mess",
+                    createdAt: "date",
+                    // Aggregation
+                    users: [], // Min users
+                    // Inside Tas
+                    columns: [], //  Columns Objects
+                    updates: [], // updates objects
+                    notes: [], // Notes objects
+                    people: [],
+                    status: '',
+                    priority: '',
+                    DueDate: '',
+                    budget: '',
+                    text: '',
+                    link: ''
+                },
+                {
+                    _id: 12333,
+                    taskTitle: "withus",
+                    createdAt: "date",
+                    // Aggregation
+                    users: [], // Min users
+                    // Inside Tas
+                    columns: [], //  Columns Objects
+                    updates: [], // updates objects
+                    notes: [], // Notes objects
+                    people: [],
+                    status: '',
+                    priority: '',
+                    DueDate: '',
+                    budget: '',
+                    text: '',
+                    link: ''
+                }
+                ], // Task object
+                color: "color",
+                lastUpdated: "",
+                type: 'status',
+                name: "name",
+                users: [{
+                    userName: "user1412",
+                    _id: "1234",
+                }, {
+                    userName: "user14as12",
+                    _id: "1234",
+                }],
+                lastUpdate: Date.now(),
                 descryption: "status was Change From",
                 nextValue: 'urgent',
                 prevValue: 'Low',
                 assignedTo: 'Abir',
                 createdBy: 'Kosta',
-                defaultMsg: 'Assigned To',
-                historyPath: 'Sunday/FrontEnd/CreatApp',
+                defaultMsg: 'status was Change From',
+                path: 'Sunday/FrontEnd/CreatApp',
+                isDone:false,
+                assosicatedTaskId:'111'
             },
             {
                 type: 'priority',
@@ -244,7 +517,7 @@ const initialState = {
                 prevValue: 'critical',
                 assignedTo: 'Abir',
                 createdBy: 'Kosta',
-                defaultMsg: 'Assigned To',
+                defaultMsg: 'Priority changed from',
                 historyPath: 'Sunday/FrontEnd/CreatApp',
             },
             {
