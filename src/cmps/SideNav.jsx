8c4c1c0043b12c68d948f0451878ed1b518b6ab5
Konstantin-Koktashev <lexxtalionis33@gmail.com>
2020-05-20 13:49:59 +0300

    changed nav

--- src/cmps/SideNav.jsx
+++ src/cmps/SideNav.jsx
@@ -8,23 +8,21 @@ export default function SideNav() {
         <img src=''></img>
         Logo
             </NavLink>
-            <NavLink to='/user/notifications' className='notifications'>
-        <img src=''></img>
-            </NavLink>
+
             <NavLink to='/user/inbox' className='inbox'>
         <img src=''></img>
+        Inbox
             </NavLink>
             <NavLink to='/user/myWeek' className='my-week'>
         <img src=''></img>
             </NavLink>
+            My Week
             <NavLink to='/user/invite' className='invite'>
-        <img src=''></img>
-            </NavLink>
-            <NavLink to='/user/search' className='search'>
         <img src=''></img>
             </NavLink>
             <NavLink to='/user/profile' className='search'>
         <img src=''></img>
+        Profile
             </NavLink>
             
         </div>
