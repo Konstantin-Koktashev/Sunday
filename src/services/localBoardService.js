647b3d7b3e53692ba0d4eef8059b1f7ab5181a53
shahar <shaharp2@gmail.com>
2020-05-22 18:17:10 +0300

    PO

--- src/services/localBoardService.js
+++ src/services/localBoardService.js
@@ -13,33 +13,104 @@ export default {
 
 }
 
-function setBoards(boards) {
-    console.log("setBoards -> boards", boards)
-    gBoards = boards
-}
 
+
+// groups //
+
+//add group
 function addGroup(board, group) {
-    // let board = getById(boardID)
+    group._id = makeId()
     board.groups.push(group)
     return board
-    // try {
-    //      saveBoard(board)
-    // } catch (error) {
-    //     console.log('Cant Add Group')
-    // }
 }
 
-function removeTask(board, taskToRemove) {
-    taskToRemove.
-        console.log('board after add task', board)
+//remove group
+function removeGroup(board, group) {
+    const gIdx = board.groups.findIndex(g => g._id === group._id)
+    board.groups.splice(gIdx, 1)
+    return board
+}
+
+//update groupName
+function changeGroupName(board, group, name) {
+    group.name = name
     return board
 }
 
 
+///////////////////////////////////////////
+
+// tasks //
+
+//add task
 function addTask(board, group, task) {
+    task._id = makeId()
     group.tasks.push(task)
     return board
 }
+
+//remove task
+function removeTask(board, group, task) {
+    const idx = group.tasks.findIndex(t => t._id === task._id)
+    group.tasks.splice(idx, 1)
+    return board
+}
+
+//update task title
+function updateTaskName(board, task, title) {
+    task.taskTitle = title
+    return board
+
+}
+
+
+///////////////////////////////////////////////////////
+
+// main cloums//  
+
+// add col
+function addColumn(board, column) {
+    column._id = makeId()
+    board.groups.forEach(group => group.tasks.push(column))
+    board.groups.forEach(group => group.tasks.forEach(task => task.push(column)))
+
+    //need to CHECK !
+    return board
+}
+
+// remove col
+
+
+function removeCol(board, column, group) {
+    const boardColIdx = board.columns.findIndex(col => col.order === column.order)
+    board.columns.forEach(col => col.splice(boardColIdx, 1))
+    const tasksColIdx = board.group[0].tasks[0].columns.findIndex(col => col.order === column.order)
+    board.groups.forEach(gr => {
+        gr.tasks.forEach(task => {
+            task.columns.splice(tasksColIdx, 1)
+        })
+    })
+    return board
+}
+
+
+// update column
+function updateColumnTitle(board , column , text){
+    column.value = text
+    return board
+}
+
+
+/////////////////////////////////////////////////////////////
+
+/// text column ///
+function updateColumnText(board, column, text) {
+    column.value = text
+    return board
+}
+
+
+/// person column ///
 
 function addPersonToColumn(board, person, column) {
     column.push(person)
@@ -47,24 +118,60 @@ function addPersonToColumn(board, person, column) {
 }
 function removePersonToTask(board, person, column) {
     const personIdx = column.findIndex(pers => pers._id === person._id)
-    column.splice(personIdx , 1)
+    column.splice(personIdx, 1)
     return board
 }
 
+
+
+// number column //
+
+// update number //
+
+function updateNumberColumn(board , column , num){
+    column.value = num
+    return board
+}
+
+
+
+// date column //
 
 function changeTaskDateColumn(board, column, date) {
     column.value = date
     return board
 }
-function changeTaskTextColumn(board, task, text) {
-    const col = task.columns.find(col => col.type === 'text')
-    col.value = text
+
+
+
+
+/// try generic
+function changeColumn(board , column , value){
+    column.value = value 
     return board
 }
+
+
+
+
+
+
+
+// function setBoards(boards) {
+//     console.log("setBoards -> boards", boards)
+//     gBoards = boards
+// }
+
+
+
+
+
+
 function changeTasklabelColumn(board, column, label) {
     column.value = label
     return board
 }
+
 
 
 
