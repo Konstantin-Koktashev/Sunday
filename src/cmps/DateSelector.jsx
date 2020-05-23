9dc562f9c90d60eb163cd3037e40689e100d2cf3
AbirNadav <pakakaw41@gmail.com>
2020-05-24 01:54:26 +0300

    123123123

--- src/cmps/DateSelector.jsx
+++ src/cmps/DateSelector.jsx
@@ -1,52 +1,37 @@
-import React, { Component } from 'react'
-import { TextField } from '@material-ui/core';
-import localBoardService from '../services/localBoardService';
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { TextField } from "@material-ui/core";
+import localBoardService from "../services/localBoardService";
+import { connect } from "react-redux";
 
-import { saveBoard , loadBoards } from '../actions/boardActions'
+import { saveBoard, loadBoards } from "../actions/boardActions";
 
-import DatePicker from 'react-datepicker'
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-
 
 class DateSelector extends Component {
   state = {
-    startDate: new Date()
+    startDate: new Date(),
   };
 
   async componentDidMount() {
-    const startDate = await this.props.column.value
-    let fixedDate =  new Date(startDate)
-    if (startDate !== 'Date') this.setState({ startDate:fixedDate })
+    const startDate = await this.props.column.value;
+    let fixedDate = new Date(startDate);
+    if (startDate !== "Date") this.setState({ startDate: fixedDate });
   }
-
 
   handleChange = async (date) => {
-    let fixedDate =  date.getTime()
-    const { currBoard, column } = this.props
-    const board = localBoardService.changeColumn(currBoard, column, fixedDate)
-    await this.props.saveBoard(board)
-    await this.props.loadBoards()
-
-
-  }
+    let fixedDate = date.getTime();
+    const { currBoard, column } = this.props;
+    const board = localBoardService.changeColumn(currBoard, column, fixedDate);
+    await this.props.saveBoard(board);
+    await this.props.loadBoards();
+  };
   render() {
-    const { column } = this.props
-    const value = (column.value === 'Date') ? (new Date()) : column.value
+    const { column } = this.props;
+    const value = column.value === "Date" ? new Date() : column.value;
     return (
       <div>
         <form className={"classes.container"} noValidate>
-<<<<<<< HEAD
-          <TextField
-            id="date"
-            type="date"
-            defaultValue={this.props.column.value}
-            className={"classes.textField"}
-            InputLabelProps={{
-              shrink: true,
-            }}
-            onInput={(ev) => this.handleChange(ev)}
-=======
           <DatePicker
             // id="date"
             // type="date"
@@ -55,10 +40,9 @@ class DateSelector extends Component {
             // InputLabelProps={{
             //   shrink: true,
             // }}
-            // placeholderText="Click to select a date" 
+            // placeholderText="Click to select a date"
             selected={this.state.startDate}
             onChange={(ev) => this.handleChange(ev)}
->>>>>>> 9bacb30650f704e3cf7b57fe5deb9736904a5d28
           />
         </form>
       </div>
@@ -67,21 +51,12 @@ class DateSelector extends Component {
 }
 
 const mapStateToProps = (state) => ({
-<<<<<<< HEAD
   currBoard: state.userBoards.currBoard,
-=======
-
-  currBoard: state.userBoards.currBoard
-  
->>>>>>> 9bacb30650f704e3cf7b57fe5deb9736904a5d28
 });
 
 const mapDispatchToProps = {
   saveBoard,
-<<<<<<< HEAD
-=======
-  loadBoards
->>>>>>> 9bacb30650f704e3cf7b57fe5deb9736904a5d28
+  loadBoards,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(DateSelector);
