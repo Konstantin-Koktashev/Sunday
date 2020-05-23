bdf7972779ef33911c5200e45e8b2f8e526e3b9a
AbirNadav <pakakaw41@gmail.com>
2020-05-23 11:51:47 +0300

    123123

--- src/services/localBoardService.js
+++ src/services/localBoardService.js
@@ -21,12 +21,38 @@ export default {
     changeColumn,
     removePersonToTask,
     changeTaskDateColumn
-
+    ,
+    sortColumnsByBox
 
 
 }
+//Sort Cols
+
+function sortColumnsByBox(board, order) {
+
+    board.groups.forEach((group) => {
+        group.tasks.forEach((task) => {
+            task.columns = _mapOrder(task.columns, order, "order");
+        });
+    });
+    debugger
+
+    return board
+};
 
 
+function _mapOrder(array, order, key) {
+    array.sort(function (a, b) {
+        var A = a[key],
+            B = b[key];
+        if (order.indexOf(A) > order.indexOf(B)) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+    return array;
+};
 
 // groups //
 
