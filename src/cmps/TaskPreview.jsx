2378ec973d59134cd362a1857b48448902c5a6b4
AbirNadav <pakakaw41@gmail.com>
2020-05-23 14:54:43 +0300

    123

--- src/cmps/TaskPreview.jsx
+++ src/cmps/TaskPreview.jsx
@@ -11,7 +11,6 @@ export function TaskPreview(props) {
   const cols = task.columns;
   SortCols(board, cols);
   function SortCols(board, cols) {
-    debugger;
     let order = [];
     board.columns.forEach((col) => {
       order.push(col.order);
