6d8eaa163143fb681735cbaaec1f16389638c137
AbirNadav <pakakaw41@gmail.com>
2020-05-28 03:12:23 +0300

    123

--- src/cmps/Board/BoardHeader.jsx
+++ src/cmps/Board/BoardHeader.jsx
@@ -2,20 +2,19 @@ import React from "react";
 import "../../style/cmps/boardHeader.css";
 import AddGroup from "../Groups/AddGroup";
 import FilterByText from "../Filters/FilterByText.jsx";
-import Swal from 'sweetalert2'
-import ConfirmDialog from '../Board/ConfirmDialog'
+import Swal from "sweetalert2";
+import ConfirmDialog from "../Board/ConfirmDialog";
 
 export default function BoardHeader(props) {
-  const confirmDelete = () =>{
-    let  boardId = props.board._id;
-    props.removeBoard(boardId)
-
-  }
+  const confirmDelete = () => {
+    let boardId = props.board._id;
+    props.removeBoard(boardId);
+  };
   const board = props.board;
   return (
     <div className="board-header-container flex a-center space-between">
-        <h2>{board.name}</h2>
-        {/* <h2> {props.user.username}</h2> */}
+      <h2>{board.name}</h2>
+      {/* <h2> {props.user.username}</h2> */}
 
       <button className="toggle-chart-btn" onClick={props.toggleChart}>
         {props.chartIsOpen ? "Board View" : "Chart View"}
@@ -24,7 +23,7 @@ export default function BoardHeader(props) {
         <div className="flex space-between">
           <AddGroup board={props.board}></AddGroup>
 
-          <ConfirmDialog  remove={confirmDelete}/>
+          <ConfirmDialog remove={confirmDelete} />
         </div>
 
         <FilterByText currBoard={props.board} />
